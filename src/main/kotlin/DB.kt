6da22e1c204d15e0bad2975f74536ba20bb51a13
Ondřej Honc <ondra.honc1@gmail.com>
2026-08-31 17:19:32 +0200

    feat: implemented init on DB object

--- src/main/kotlin/DB.kt
+++ src/main/kotlin/DB.kt
@@ -1,4 +1,34 @@
 package url.shortener
 
-class DB {
-}
+import java.sql.DriverManager
+import java.sql.Connection
+import java.sql.PreparedStatement
+import java.sql.ResultSet
+
+
+object DB {
+    init {
+        DriverManager.getConnection("jdbc:sqlite:shortener.db").use { connection ->
+            val sqlString = """CREATE TABLE IF NOT EXISTS links (key TEXT PRIMARY KEY, original_url TEXT NOT NULL)"""
+            updateSQL(sqlString, connection)
+        }
+    }
+
+    fun add(originalUrl: String, key: String): Boolean {
+        return false
+    }
+
+    fun find(key: String): ShortURL {
+
+    }
+
+    private fun updateSQL(sql: String, connection: Connection) {
+        connection.createStatement().use { statement ->
+            statement.executeUpdate(sql)
+        }
+    }
+
+    private fun executeUpdate(sql: String, vararg params: String): Boolean {
+        return false
+    }
+} 
